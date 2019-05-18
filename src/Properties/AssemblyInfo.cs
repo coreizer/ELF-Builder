669941e150fb566e643ed3b27abd910deaf62474
AlphaNyne <alphanyne@outlook.com>
2019-05-19 03:25:16 +0900

    ライセンスヘッダー追加

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2017 I4MBPro
+ * Copyright (c) 2019 AlphaNyne
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
